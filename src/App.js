ce14aea70a2b84fc00ad3ac2a1742e4d55e23926
Bryan Herrera <bcherrera14@gmail.com>
2022-07-18 18:20:24 -0700

    add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-	return <div>Hello from app!</div>;
+	return (
+		<React.Fragment>
+			<Header />
+			<h1>Hello!</h1>
+			<p>This is my feedback app</p>
+		</React.Fragment>
+	);
 }
 
 export default App;
