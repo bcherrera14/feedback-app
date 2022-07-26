ee35722aa58514267eed88b1fd730b658fc4a528
Bryan Herrera <bcherrera14@gmail.com>
2022-07-25 18:10:51 -0700

    add abput link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
 	const [ feedback, setFeedback ] = useState(FeedbackData);
@@ -41,6 +42,7 @@ function App() {
 					/>
 					<Route path="/about" element={<AboutPage />} />
 				</Routes>
+				<AboutIconLink />
 			</div>
 		</Router>
 	);
