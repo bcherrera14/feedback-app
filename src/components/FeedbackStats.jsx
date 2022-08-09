a53589c7ee10104dbec3d72aa5eef4ebd0ea291b
Bryan Herrera <bcherrera14@gmail.com>
2022-08-09 07:13:51 -0700

    update and delete items

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -8,9 +8,8 @@ function FeedbackStats() {
 	//Calc Rating Average
 	let average =
 		feedback.reduce((acc, cur) => {
-			return acc + cur.rating;
+			return acc + parseInt(cur.rating);
 		}, 0) / feedback.length;
-
 	average = average.toFixed(1).replace(/[.,]0$/, '');
 
 	return (
